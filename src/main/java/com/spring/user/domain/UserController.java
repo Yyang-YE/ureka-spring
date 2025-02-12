7146a64a2bb3a80bb7bdc35f821ac8f5e47c7077
Yeoeun Yang <yyang78077@naver.com>
2025-02-12 21:07:40 +0900

    TEST

---
+++ src/main/java/com/spring/user/domain/UserController.java
@@ -0,0 +1,14 @@
+package com.spring.user.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @GetMapping
+    public String showAny() {
+        return "안녕하세요, 화면입니다.";
+    }
+}
